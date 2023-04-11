7a88627c92226a5b15fdedc91ebfd2300084fde4
aditigupta <aditi.gupta@deeporion.com>
2023-04-12 00:04:55 +0530

    edit button added

--- index.js
+++ index.js
@@ -8,28 +8,35 @@ function addMore() {
     var box = document.getElementById("box");
     let li = document.createElement("li");
 
-    // content.id = "data";
     box.appendChild(li);
     document.getElementById("name").value = "";
     var content = document.createElement("span");
-    content.textContent = name;
+    content.innerText = name;
+    content.className = "getData";
+    content.id = "data";
     li.appendChild(content);
-
+    //creating a delete button
     let delButton = document.createElement("button");
     delButton.textContent = "delete";
     delButton.type = "button";
+    delButton.id = "remo";
     delButton.className = "remove";
     li.appendChild(delButton);
     delButton.addEventListener("click", deleting);
 
-    //for editing
+    //create an edit button
     let edit = document.createElement("button");
     edit.textContent = "Edit";
     edit.type = "button";
+    edit.id = "update";
     edit.className = "edit";
     li.appendChild(edit);
-    edit.addEventListener("click", (e) => {
-      editing(e.target.parentElement);
+
+    edit.addEventListener("click", () => {
+      let name = document.querySelector("#name");
+      name.value = content.innerText;
+      const parent = edit.parentElement;
+      parent.parentElement.removeChild(parent);
     });
   }
 }
@@ -37,17 +44,4 @@ let delbtn = document.querySelector("ul");
 function deleting() {
   let forDelete = this.parentElement;
   delbtn.removeChild(forDelete);
-}
-
-function editing(a) {
-  var update = a.querySelector("span");
-  let input = document.createElement("input");
-  input.type = "text";
-  input.value = update.textContent;
-  a.append(input);
-  input.focus();
-  input.addEventListener("blur", () => {
-    update.innerHTML = input.value;
-    input.remove();
-  });
 }
