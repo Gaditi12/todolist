80971f417cab6c0395708d0bd3eb6d2769163d7c
aditigupta <aditi.gupta@deeporion.com>
2023-04-13 10:40:11 +0530

    added functionality in edit button

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var isEdit = 0;
+var editID = "";
 function addMore() {
   var error = document.getElementById("error");
   error.innerHTML = "";
@@ -5,57 +7,70 @@ function addMore() {
   if (name == "") {
     error.innerHTML = "Please add task";
   } else {
-    var box = document.getElementById("box");
-    let li = document.createElement("li");
-    li.className = "editMode";
-    box.appendChild(li);
+    if (isEdit == 1) {
+      let num = document.querySelector("#" + editID);
+      num.innerText = name;
+      //var contentValue = document.getElementById("data_" + uniqueID);
+      //contentValue.innerText = name.value;
+      isEdit = 0;
+      editID = "";
+      document.getElementById("name").value = "";
+    } else {
+      var lengthOfUl = document
+        .getElementById("box")
+        .getElementsByTagName("li").length;
+      var uniqueID = lengthOfUl + 1;
+      var box = document.getElementById("box");
+      let li = document.createElement("li");
+      li.className = "editMode";
+      box.appendChild(li);
 
-    var checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.className = "check-box";
-    li.appendChild(checkbox);
+      console.log(uniqueID);
+      var checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.className = "check-box";
+      li.appendChild(checkbox);
 
-    document.getElementById("name").value = "";
-    var content = document.createElement("span");
-    content.innerText = name;
-    content.className = "getData";
-    content.id = "data";
-    li.appendChild(content);
-    //creating a delete button
-    let delButton = document.createElement("button");
-    delButton.textContent = "delete";
-    delButton.type = "button";
-    delButton.id = "remo";
-    delButton.className = "remove";
-    li.appendChild(delButton);
-    delButton.addEventListener("click", deleting);
+      document.getElementById("name").value = "";
+      var content = document.createElement("span");
+      content.innerText = name;
+      content.className = "getData";
+      content.id = "data" + "_" + uniqueID;
 
-    //create an edit button
-    let edit = document.createElement("button");
-    edit.textContent = "Edit";
-    edit.type = "button";
-    edit.id = "update";
-    edit.className = "edit";
-    edit.disabled = false;
-    li.appendChild(edit);
+      li.appendChild(content);
+      //creating a delete button
+      let delButton = document.createElement("button");
+      delButton.textContent = "delete";
+      delButton.type = "button";
+      delButton.id = "remo";
+      delButton.className = "remove";
+      li.appendChild(delButton);
+      delButton.addEventListener("click", deleting);
 
-    edit.addEventListener("click", () => {
-      let name = document.querySelector("#name");
-      //let btn = document.querySelector("#button");
-      name.value = content.innerText;
+      //create an edit button
+      let edit = document.createElement("button");
+      edit.textContent = "Edit";
+      edit.type = "button";
+      edit.id = "update";
+      edit.className = "edit";
+      edit.disabled = false;
+      li.appendChild(edit);
 
-      //btn.value = "Save";
-
-      // const parent = edit.parentElement;
-      // parent.parentElement.removeChild(parent);
-    });
-    checkbox.addEventListener("click", () => {
-      if (checkbox.checked == true) {
-        edit.disabled = true;
-      } else {
-        edit.disabled = false;
-      }
-    });
+      edit.addEventListener("click", () => {
+        let name = document.querySelector("#name");
+        var contentValue = document.getElementById("data_" + uniqueID);
+        name.value = contentValue.innerText;
+        isEdit = 1;
+        editID = "data_" + uniqueID;
+      });
+      checkbox.addEventListener("click", () => {
+        if (checkbox.checked == true) {
+          edit.disabled = true;
+        } else {
+          edit.disabled = false;
+        }
+      });
+    }
   }
 }
 let delbtn = document.querySelector("ul");
